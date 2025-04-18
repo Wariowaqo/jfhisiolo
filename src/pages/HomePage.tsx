0eba2093c2225b8c799cf2b0aec7067c1f8b3d3f
WarioWW <62429450+Wariowaqo@users.noreply.github.com>
2025-04-18 12:50:12 +0200

    nav

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -32,7 +32,7 @@ const HomePage: React.FC = () => {
   className="hero-section h-screen pt-16 relative flex items-center justify-center"
   style={{
     backgroundImage: `url('/lovable-uploads/e744e3b8-6930-4922-923d-98edc5825a96.png')`,
-    backgroundSize: 'contain',
+    backgroundSize: 'cover',
     backgroundPosition: 'center'
   }}
       >
